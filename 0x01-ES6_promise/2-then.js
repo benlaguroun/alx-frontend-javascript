094d8a6adaf9d72f3e1c6f19f9af1066913eaebd
benlaguroun <benlaguroun@gmail.com>
2024-03-05 19:04:11 +0000

    task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+// 2-then.js
+export default function handleResponseFromAPI(promise) {
+  const obj = { status: 200, body: 'success' };
+  return promise
+    .then(() => obj)
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
+}
+
