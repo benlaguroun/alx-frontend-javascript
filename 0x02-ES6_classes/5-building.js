ec0bf5fa0b2e5498bd8472d5987fd521f9fc961f
benlaguroun <benlaguroun@gmail.com>
2024-03-08 13:33:40 +0000

    task5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,16 @@
-class Building {
+export default class Building {
   constructor(sqft) {
+    if (
+      new.target !== Building && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
 }
-
-export default Building;
-
