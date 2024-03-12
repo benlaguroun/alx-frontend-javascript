805b02a01af8300e6108dc9c58f1d54366609716
benlaguroun <benlaguroun@gmail.com>
2024-03-12 11:24:32 +0000

    task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+function cleanSet(set, startString) {
+    let result = [...set]
+        .filter(value => value.startsWith(startString))
+        .map(value => value.slice(startString.length))
+        .join("-");
+    return result;
+}
+
+export default cleanSet;
+
