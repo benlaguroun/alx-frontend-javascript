7b0c03558b1501f9274bd43c0573d7255a288dfd
benlaguroun <benlaguroun@gmail.com>
2024-03-12 21:49:23 +0000

    tasl 100

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,17 @@
+export const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+    if (!weakMap.has(endpoint)) {
+        weakMap.set(endpoint, 1);
+    } else {
+        let count = weakMap.get(endpoint);
+        if (count >= 5) {
+            throw new Error('Endpoint load is high');
+        } else {
+            weakMap.set(endpoint, count + 1);
+        }
+    }
+}
+
+export { queryAPI, weakMap };
+
