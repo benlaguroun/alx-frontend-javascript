def51aef5181a8ed6b9df660fc482eea39eef04b
benlaguroun <benlaguroun@gmail.com>
2024-03-12 11:28:55 +0000

    task8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,14 @@
-function cleanSet(set, startString) {
-    let result = [...set]
-        .filter(value => value.startsWith(startString))
-        .map(value => value.slice(startString.length))
-        .join("-");
-    return result;
+export default function cleanSet(set, startString) {
+  let text = '';
+  const array = [];
+
+  if (startString && typeof startString === 'string') {
+    for (const element of set) {
+      if (element && element.startsWith(startString)) {
+        array.push(element.slice(startString.length));
+      }
+    }
+    text = array.join('-');
+  }
+  return text;
 }
-
-export default cleanSet;
-
